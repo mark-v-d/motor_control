776ab0a0c056b9cd5781caf9e929700ef327bd17
Mark <mark.vandoesburg@hetnet.nl>
2023-11-26 19:45:43 +0100

    Allow simulation of the controller.

--- software/rt/test_settings.cpp
+++ software/rt/test_settings.cpp
@@ -8,12 +8,21 @@ ss_t<order,inputs,outputs> controller;
 
 int main(int argc, char **argv)
 {
-	std::ifstream settings(argv[1]);
-	if(!settings) {
-	    perror(argv[1]);
-	    return 1;
-	}
+    std::ifstream settings(argv[1]);
+    if(!settings) {
+	perror(argv[1]);
+	return 1;
+    }
 
-	controller.read(settings);
-	return 0;
+    controller.read(settings);
+
+    std::string s;
+    while(std::getline(std::cin,s)) {
+	if(!s.size() || s[0]=='#')
+	    continue;
+	decltype(controller)::input_t input;
+	read(input,s);
+	auto output=controller.compute(input);
+	std::cout << output << std::endl;
+    }
 }
